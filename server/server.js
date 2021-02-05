a2c1063662d08b729cab64bc95a4c09a63687acd
Jeanndo <ukjeando@gmail.com>
2021-02-05 03:28:07 +0200

    updating server/server.js

--- server/server.js
+++ server/server.js
@@ -4,10 +4,8 @@ import mongoose from "mongoose";
 
 dotenv.config({ path: "./server/config.env" });
 
-
-const port = process.env.PORT;
-
 const DB = process.env.DATABASE.replace('<PASSWORD>',process.env.DATABASE_PASSWORD);
+
 mongoose.connect(DB,{
     useNewUrlParser: true,
     useCreateIndex: true,
@@ -15,6 +13,8 @@ mongoose.connect(DB,{
     useFindAndModify: false,
   }).then(()=> console.log(" Db connection done successfully"));
 
+
+const port = process.env.PORT||3500;
 const server = app.listen(port, () =>
   process.stdout.write(`Listening on port ${port} ...\n******************** \n`)
 );
